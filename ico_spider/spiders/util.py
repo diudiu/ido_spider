d4f3176cd2b8451c2ac33c6033a94ed2894c4248
Hongjie Dong <hongjieuf@gmail.com>
2018-03-05 22:13:31 -0800

    make sure start and end date of ico is crawled

---
+++ ico_spider/spiders/util.py
@@ -0,0 +1,32 @@
+import datetime
+import pytz
+
+
+# convert date string in format like 3 March 2017, '%d %b %Y' from pacific time to UTC
+# example (parseDateStringToUTC('3 Mar 2014','%d %b %Y'))
+
+def parseDateStringToUTC(date, formater):
+    try:
+        timezone = pytz.timezone("America/Los_Angeles")
+        dateObj = datetime.datetime.strptime(date, formater)
+        endDate_utc = timezone.localize(dateObj).astimezone(pytz.UTC)
+        return endDate_utc.strftime("%Y-%m-%dT%H:%M:%S")
+    except ValueError as error:
+        print ('error parsing ' + date, error)
+        return None
+
+
+def UTCDateStringToDateObj(date):
+    try:
+        return datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+    except ValueError as error:
+        print ('error parsing ' + date, error)
+        return None
+
+
+def parseDateStringToDateObj(date, formatter):
+    try:
+        return datetime.datetime.strptime(date, formatter)
+    except ValueError as error:
+        print ('error parsing ' + date, error)
+        return None
