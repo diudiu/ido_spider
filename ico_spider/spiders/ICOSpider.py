bbdc44d29846d65204aa620a2a1d51a3b1968587
Hongjie Dong <hongjieuf@gmail.com>
2018-03-05 23:12:55 -0800

    把不同的section爬取放到相应method里增加可读性

--- ico_spider/spiders/ICOSpider.py
+++ ico_spider/spiders/ICOSpider.py
@@ -63,47 +63,8 @@ class ICOSpider(BaseSpider):
                     elif 'ended-ico' in response.url:
                         item['status'] = 'ended'
                     # we need to recheck this logic
-                    # 在这里爬开始结束时间的原因是这里的时间稍微准确点
-                    timezone = pytz.timezone("America/Los_Angeles")
-                    mydatetime = datetime.datetime.now()
-                    if item['status'] == 'active':
-                        date = icoItem.xpath('.//div[@class="date"]/@data-date')[0].extract()
-                        item['endTime'] = util.parseDateStringToUTC(date, "%Y-%m-%d %H:%M:%S")
-                    elif item['status'] == 'upcoming':
-                        date = icoItem.xpath('.//div[@class="date"]/text()')[0].extract().strip()
-                        if date and "TBA" not in date:
-                            # date could be 'in 30h' or '14 March'
-                            startDate = None
-                            if date.startswith('in'):
-                                diff = re.findall(r'\d+', date)[0]
-                                if date.endswith('h'):
-                                    startDate = mydatetime + datetime.timedelta(hours=int(diff))
-                                elif date.endswith('m') or date.endswith('min'):
-                                    startDate = mydatetime + datetime.timedelta(minutes=int(diff))
-                            else:
-                                if 'TBA' not in date:
-                                    dateObj = util.parseDateStringToDateObj(date + ' ' + str(mydatetime.year),
-                                                                            '%d %b %Y')
-                                    if dateObj and dateObj < mydatetime:
-                                        next_year = str(mydatetime.year + 1)
-                                        dateObj = datetime.datetime.strptime(date + ' ' + next_year, '%d %b %Y')
-                                    startDate = dateObj
-                                else:
-                                    startDate = None
-
-                            if startDate:
-                                startDate_utc = timezone.localize(startDate).astimezone(pytz.UTC)
-                                item['startTime'] = startDate_utc.strftime("%Y-%m-%dT%H:%M:%S")
-                    elif item['status'] == 'ended':
-                        date = icoItem.xpath('normalize-space(.//div[@class="date"])')[0].extract().split(':')[
-                            -1].strip()
-                        if date and "TBA" not in date:
-                            # date is like '14 March'
-                            utc_date = util.parseDateStringToUTC(date + ' ' + str(mydatetime.year), '%d %b %Y')
-                            if utc_date and util.UTCDateStringToDateObj(utc_date) > mydatetime:
-                                last_year = str(mydatetime.year - 1)
-                                utc_date = util.parseDateStringToUTC(date + ' ' + last_year, '%d %b %Y')
-                            item['endTime'] = utc_date
+                    # 在这里爬开始结束时间的原因是这里的时间稍微准确点（倒计时或者含有小时级别时间的数据）,如果这里爬不到才去details page
+                    self._parse_ico_list_page_start_end_time(icoItem, item)
                     yield Request(path, callback=self.parse_ico_drops_items, meta={'item': item})
 
         self.log('A response from %s just arrived!' % response.url)
@@ -128,7 +89,6 @@ class ICOSpider(BaseSpider):
                         hash_object.update(img)
                         hex_dig = hash_object.hexdigest()
                         item['avatar'] = hex_dig + '.' + img.split('.')[-1]
-                    mainInfo = section.xpath('.//div[@class="ico-icon"]')[0]
                     description = \
                         section.xpath('normalize-space(.//div[@class="ico-description"]/text())')[0].extract()[
                             0].strip()
@@ -142,26 +102,8 @@ class ICOSpider(BaseSpider):
                     if len(titleDiv):
                         title = titleDiv[0].extract()
                         if 'token' in title.lower() and 'sale' in title.lower():  # financial section
-                            start_end_date = section.xpath(
-                                'normalize-space(.//i[contains(@class,"fa-calendar")]/following-sibling::*[1])')[
-                                0].extract()
-                            if "PERIOD ISN'T SET" in start_end_date:
-                                item['startTime'] = None
-                                item['endTime'] = None
-                            else:
-                                start_end_date = start_end_date.split(u"\u2013")
-                                if len(start_end_date) > 1:
-                                    year = datetime.datetime.now().year
-                                    startDate = util.parseDateStringToUTC(
-                                        start_end_date[0].split(':')[-1].strip() + ' ' + str(year), '%d %b %Y')
-                                    endDate = util.parseDateStringToUTC(start_end_date[1].strip() + ' ' + str(year),
-                                                                        '%d %b %Y')
-                                    print ('start-end ' + startDate + " - " + endDate)
 
-                                    if not item.get('startTime', None):
-                                        item['startTime'] = startDate
-                                    if not item.get('endTime', None):
-                                        item['endTime'] = endDate
+                            self._parse_details_page_start_end_time(section, item)
                             self._parse_financial(section, item)
 
                         if 'our rating' in title.lower():  # our rating section
@@ -293,3 +235,67 @@ class ICOSpider(BaseSpider):
             elif 'max' in key.lower() and 'min' in key.lower() and 'cap' in key.lower():
                 financial_item['minPersonalCap'] = value.split('/')[1].strip()
                 financial_item['maxPersonalCap'] = value.split('/')[1].strip()
+
+    def _parse_details_page_start_end_time(self, section, item):
+        start_end_date = section.xpath(
+            'normalize-space(.//i[contains(@class,"fa-calendar")]/following-sibling::*[1])')[
+            0].extract()
+        if "PERIOD ISN'T SET" in start_end_date:
+            item['startTime'] = None
+            item['endTime'] = None
+        else:
+            start_end_date = start_end_date.split(u"\u2013")
+            if len(start_end_date) > 1:
+                year = datetime.datetime.now().year
+                startDate = util.parseDateStringToUTC(
+                    start_end_date[0].split(':')[-1].strip() + ' ' + str(year), '%d %b %Y')
+                endDate = util.parseDateStringToUTC(start_end_date[1].strip() + ' ' + str(year),
+                                                    '%d %b %Y')
+                print ('start-end ' + startDate + " - " + endDate)
+
+                if not item.get('startTime', None):
+                    item['startTime'] = startDate
+                if not item.get('endTime', None):
+                    item['endTime'] = endDate
+
+    def _parse_ico_list_page_start_end_time(self, icoItem, item):
+        timezone = pytz.timezone("America/Los_Angeles")
+        mydatetime = datetime.datetime.now()
+        if item['status'] == 'active':
+            date = icoItem.xpath('.//div[@class="date"]/@data-date')[0].extract()
+            item['endTime'] = util.parseDateStringToUTC(date, "%Y-%m-%d %H:%M:%S")
+        elif item['status'] == 'upcoming':
+            date = icoItem.xpath('.//div[@class="date"]/text()')[0].extract().strip()
+            if date and "TBA" not in date:
+                # date could be 'in 30h' or '14 March'
+                startDate = None
+                if date.startswith('in'):
+                    diff = re.findall(r'\d+', date)[0]
+                    if date.endswith('h'):
+                        startDate = mydatetime + datetime.timedelta(hours=int(diff))
+                    elif date.endswith('m') or date.endswith('min'):
+                        startDate = mydatetime + datetime.timedelta(minutes=int(diff))
+                else:
+                    if 'TBA' not in date:
+                        dateObj = util.parseDateStringToDateObj(date + ' ' + str(mydatetime.year),
+                                                                '%d %b %Y')
+                        if dateObj and dateObj < mydatetime:
+                            next_year = str(mydatetime.year + 1)
+                            dateObj = datetime.datetime.strptime(date + ' ' + next_year, '%d %b %Y')
+                        startDate = dateObj
+                    else:
+                        startDate = None
+
+                if startDate:
+                    startDate_utc = timezone.localize(startDate).astimezone(pytz.UTC)
+                    item['startTime'] = startDate_utc.strftime("%Y-%m-%dT%H:%M:%S")
+        elif item['status'] == 'ended':
+            date = icoItem.xpath('normalize-space(.//div[@class="date"])')[0].extract().split(':')[
+                -1].strip()
+            if date and "TBA" not in date:
+                # date is like '14 March'
+                utc_date = util.parseDateStringToUTC(date + ' ' + str(mydatetime.year), '%d %b %Y')
+                if utc_date and util.UTCDateStringToDateObj(utc_date) > mydatetime:
+                    last_year = str(mydatetime.year - 1)
+                    utc_date = util.parseDateStringToUTC(date + ' ' + last_year, '%d %b %Y')
+                item['endTime'] = utc_date
